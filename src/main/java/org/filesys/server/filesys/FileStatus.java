6daa87c8b2ce22197ffa57efeeb4fff1b0183e27
Gary Spencer <gk.spencer@me.com>
2018-10-03 11:21:24 +0100

    Removed old commented out code.

--- src/main/java/org/filesys/server/filesys/FileStatus.java
+++ src/main/java/org/filesys/server/filesys/FileStatus.java
@@ -30,43 +30,4 @@ public enum FileStatus {
     NotExist,
     FileExists,
     DirectoryExists
-
-    // File status constants
-//    public final static int Unknown         = -1;
-//    public final static int NotExist        = 0;
-//    public final static int FileExists      = 1;
-//    public final static int DirectoryExists = 2;
-    
-//    public final static int MaxStatus       = 2;
-
-    /**
-     * Return the file status as a string
-     *
-     * @param sts int
-     * @return String
-     */
-/*
-    public final static String asString(int sts) {
-
-        // Convert the status to a string
-        String ret = "";
-
-        switch (sts) {
-            case Unknown:
-                ret = "Unknown";
-                break;
-            case NotExist:
-                ret = "NotExist";
-                break;
-            case FileExists:
-                ret = "FileExists";
-                break;
-            case DirectoryExists:
-                ret = "DirExists";
-                break;
-        }
-
-        return ret;
-    }
-*/
 }
