ca4a6f1a2585150548812a32bd1c35b25f30262d
Gary Spencer <gk.spencer@me.com>
2020-08-03 10:22:00 +0100

    Fixed an issue where the required features and supported features masks were checked incorrectly

--- src/main/java/org/filesys/server/filesys/db/JdbcDBInterface.java
+++ src/main/java/org/filesys/server/filesys/db/JdbcDBInterface.java
@@ -163,9 +163,9 @@ public abstract class JdbcDBInterface implements DBInterface, DBConnectionPoolLi
         EnumSet<Feature> supFeatures = getSupportedFeatures();
 
         //	Check if there are any unsupported features requested
-        for ( Feature curFeature : supFeatures)
-            if ( featureMask.contains( curFeature) == false)
-            throw new DBException("Unsupported feature requested (" + curFeature.name() + ")");
+        for ( Feature curFeature : featureMask)
+            if ( supFeatures.contains( curFeature) == false)
+                throw new DBException("Unsupported feature requested (" + curFeature.name() + ")");
 
         //	Set the requested features
         m_reqFeatures = featureMask;
